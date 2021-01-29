f2d0d03b372a139ce208943c308412e56aa193fe
jiyoung-99 <1skylar1111@gmail.com>
2021-01-30 00:01:17 +0900

    movie

--- Movie-seat-Booking/script.js
+++ Movie-seat-Booking/script.js
@@ -4,8 +4,70 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
-const ticketPrice = +movieSelect.value;
 
+populateUI();
+
+let ticketPrice = +movieSelect.value;
+
+//Initial count and total set
+updateSelectedCount();
+
+// Save selected movie index and pricd
+function setMovideData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
+// Get data from localstorage and populate UI
+function populateUI () {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+    if(selectedSeats !== null && selectedSeats.length >0 ) {
+        seats.forEach((seat, index) => {
+            // console.log(seat, '----', index)
+            if(selectedSeats.indexOf(index) > -1){
+                seat.classList.add('selected');
+            }
+        })
+
+    }
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+    if(selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
+}
+
+
+//functions
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+    // Copy selected seats into arr
+    // Map through array
+    // return a new array 
+    const seatsIndex = [...selectedSeats].map((seat) => {
+        return [...seats].indexOf(seat);
+    })
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
+    const selectedSeatsCount = selectedSeats.length;
+    
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+
+///event listener
+
+//movie select event
+movieSelect.addEventListener('change', e => {
+    ticketPrice = +e.target.value;
+    setMovideData(e.target.selectedIndex, e.target.value);
+    updateSelectedCount();
+})
+
+//seat click event
 container.addEventListener('click', (e) => {
     if(e.target.classList.contains('seat') &&
     !e.target.classList.contains('occupied')) {
@@ -14,13 +76,3 @@ container.addEventListener('click', (e) => {
         updateSelectedCount();
     }
 })
-
-
-//functions
-function updateSelectedCount() {
-    const selectedSeats = document.querySelectorAll('.row .seat.selected');
-    const selectedSeatsCount = selectedSeats.length;
-    
-    count.innerText = selectedSeatsCount;
-    total.innerText = selectedSeatsCount * ticketPrice;
-}
