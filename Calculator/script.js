084e14e81ba6ee49d40ff3036e1c56963c0b281f
jiyoung-99 <1skylar1111@gmail.com>
2021-01-30 19:28:04 +0900

    calculator

---
+++ Calculator/script.js
@@ -0,0 +1,45 @@
+// function caclulate() {
+//     fetch('items.json')
+//     .then(res => res.json())
+//     .then(data => document.body.innerHTML = data[0].text)
+// }
+
+const currencyEl_one = document.getElementById('currency-one');
+const amountEl_one = document.getElementById('amount-one');
+const currencyEl_two = document.getElementById('currency-two');
+const amountEl_two = document.getElementById('amount-two');
+
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+//Fetch exchange rates and update the DOM
+function caclulate() {
+    const currency_one = currencyEl_one.value;
+    const currency_two = currencyEl_two.value;
+
+    fetch(`https://v6.exchangerate-api.com/v6/223903111018577041f80fed/latest/${currency_one}`)
+    .then(res => res.json())
+    .then(data => {
+        // console.log(data.conversion_rates);
+        const rate = data.conversion_rates[currency_two];
+        // console.log(rate);
+        rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`
+
+        amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+    })
+}
+
+//Event Listeners
+currencyEl_one.addEventListener('change', caclulate);
+amountEl_one.addEventListener('input', caclulate);
+currencyEl_two.addEventListener('change', caclulate);
+amountEl_two.addEventListener('input', caclulate);
+
+swap.addEventListener('click', () => {
+    const temp = currencyEl_one.value;
+    currencyEl_one.value = currencyEl_two.value;
+    currencyEl_two.value = temp;
+    caclulate();
+});
+
+caclulate();
